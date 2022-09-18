ce79a40f1de77ac3e1f95375920d6e3d164eb292
Ibrohim Bultakov <bii23.uz@gmail.com>
2022-09-18 12:12:06 +0500

    Update utils.py

--- app/utils.py
+++ app/utils.py
@@ -7,6 +7,7 @@ from openpyxl.writer.excel import save_virtual_workbook
 def data_to_xslx(user_data: list):
     wb = Workbook()
     ws = wb.active
+
     ws.auto_filter.ref = f"A1:D{len(user_data) + 1}"  # filter
     names: dict = {"A1": "ID", "B1": "Name", "C1": "Phone", "D1": "About"}
     for key, value in names.items():
@@ -14,11 +15,11 @@ def data_to_xslx(user_data: list):
         cl.value = value
         cl.alignment = Alignment(horizontal="center", vertical="center")
         cl.font = Font(b=True)
-    for _, user in enumerate(user_data, start=2):
-        ws[f"A{_}"] = _
-        ws[f"B{_}"] = user.name
-        ws[f"C{_}"] = user.phone
-        ws[f"D{_}"] = user.about
+
+    for i, user in enumerate(user_data, start=2):
+        for j, data in enumerate(user, start=1):
+            cr = ws.cell(row=i, column=j)
+            cr.value = data
     response = HttpResponse(content=save_virtual_workbook(wb), content_type='application/xlsx')
     response['Content-Disposition'] = 'attachment; filename=user_data.xlsx'
     return response
